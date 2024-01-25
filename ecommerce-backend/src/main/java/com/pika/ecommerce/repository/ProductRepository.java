2cc10d2a26b3789460901b8a61ebe65b564ca389
AyeChanHtunNaing <ayechanhtunnaing@icloud.com>
2024-01-25 08:46:26 +0630

    feat : Product API

---
+++ ecommerce-backend/src/main/java/com/pika/ecommerce/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.pika.ecommerce.repository;
+
+import com.pika.ecommerce.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+}
---
+++ ecommerce-backend/src/main/java/com/pika/ecommerce/service/ProductService.java
@@ -0,0 +1,51 @@
+package com.pika.ecommerce.service;
+
+
+import com.pika.ecommerce.dto.product.ProductDto;
+import com.pika.ecommerce.model.Category;
+import com.pika.ecommerce.model.Product;
+import com.pika.ecommerce.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<ProductDto> listProducts() {
+        List<Product> products = productRepository.findAll();
+        List<ProductDto> productDtos = new ArrayList<>();
+        for(Product product : products) {
+            productDtos.add(new ProductDto(product));
+        }
+        return productDtos;
+    }
+
+    public static Product getProductFromDto(ProductDto productDto, Category category) {
+        Product product = new Product();
+        product.setCategory(category);
+        product.setDescription(productDto.getDescription());
+        product.setImageURL(productDto.getImageURL());
+        product.setPrice(productDto.getPrice());
+        product.setName(productDto.getName());
+        return product;
+    }
+
+
+    public void addProduct(ProductDto productDto, Category category) {
+        Product product = getProductFromDto(productDto, category);
+        productRepository.save(product);
+    }
+
+    public void updateProduct(Integer productID, ProductDto productDto, Category category) {
+        Product product = getProductFromDto(productDto, category);
+        product.setId(productID);
+        productRepository.save(product);
+    }
+}
